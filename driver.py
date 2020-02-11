fcaae7904fbe5a5593e8656ba5165ebdfe524679
erssmith <erssmith@umich.edu>
2020-02-10 20:37:57 -0500

    added filter output

--- driver.py
+++ driver.py
@@ -1,17 +1,15 @@
 import smbus
 import time as t
 import numpy as np
-# import matplotlib.pyplot as plt
 from madgwickahrs import MadgwickAHRS
 
 I2C_IMU_ADDRESS = 0x69
 bus = smbus.SMBus(2)
 
-#filter = MadgwickAHRS()
+filter = MadgwickAHRS()
 
 def get_decimal(ls, ms):
     high = read_data(ms) << 8
-
     return np.int16((high | read_data(ls)))
 
 def read_data(num):
@@ -30,28 +28,23 @@ def get_data():
     gyro_y = get_decimal(0x36, 0x35)
     gyro_z = get_decimal(0x38, 0x37)
 
+    # all magnetometer data returns 0 because it is not being read correctly
     mag_x = get_decimal(0x11, 0x12)
     mag_y = get_decimal(0x13, 0x14)
     mag_z = get_decimal(0x15, 0x16)
 
-    # print(accel_x)
-
     return np.array([accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z])
 
-def moving_average(a, n=10):
-    ret = np.cumsum(a, dtype=float)
-    ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1:] / n
-
 def main():
-    bus.write_byte_data(I2C_IMU_ADDRESS, 0x06, 0x01) # wake up imu from sleep, try until works
-    bus.write_byte_data(I2C_IMU_ADDRESS, 0x31, 0x02) # wake magnetometer from sleep, try until works
+    success = False
+    while not success:
+        try:
+            bus.write_byte_data(I2C_IMU_ADDRESS, 0x06, 0x01) # wake up imu from sleep, try until works
+            bus.write_byte_data(I2C_IMU_ADDRESS, 0x31, 0x02) # wake magnetometer from sleep, try until works
+            success = True
+        except:
+            pass
 
-    time = np.array([])
-    roll = np.array([])
-    pitch = np.array([])
-    yaw = np.array([])
-    
     while(True):
         try:
             data = get_data()
@@ -59,27 +52,28 @@ def main():
             print("Connection Lost")
             t.sleep(1)
  
-        print("Accel: ", data[0], ",", data[1], ",", data[2])
-        print("Gyro: ", data[3], ",", data[4], ",", data[5])
-        print("Mag: ", data[6], ",", data[7], ",", data[8])
-        print()
+        # print("Accel: ", data[0], ",", data[1], ",", data[2])
+        # print("Gyro: ", data[3], ",", data[4], ",", data[5])
+        # print("Mag: ", data[6], ",", data[7], ",", data[8])
+        # print()
 
-        
-        # acc = np.array([data[0], data[1], data[2]])
-        # gyr = np.array([data[3], data[4], data[5]])
-        # mag = np.array([data[6], data[7], data[8])])
-        # gyr_rad = gyr * (np.pi/180)
+        acc = np.array([data[0], data[1], data[2]])
+        gyr = np.array([data[3], data[4], data[5]])
+        mag = np.array([data[6], data[7], data[8]])
+        gyr_rad = gyr * (np.pi/180)
     
         # filter.update(gyr_rad,acc,mag)
-        # ahrs = filter.quaternion.to_euler_angles()
-        # curRoll = ahrs[0]
-        # curPitch = ahrs[1]
-        # curYaw = ahrs[2]
+        # aboves update method can be run instead of update_imu if the magnetometer problem is fixed 
+        filter.update_imu(gyr_rad,acc) #updates the filter and returns roll, pitch, and yaw in quaternion form
+        ahrs = filter.quaternion.to_euler_angles()
 
-        # time = np.append(time, np.array([elapsed_time]))
-		# roll = np.append(roll, np.array([curRoll]))
-		# pitch = np.append(pitch, np.array([curPitch]))
-		# yaw = np.append(yaw, np.array([curYaw]))
+        # values are between -pi and pi
+        curRoll = ahrs[0]
+        curPitch = ahrs[1]
+        curYaw = ahrs[2]
+        
+        print("Roll: ", curRoll, " Pitch: ", curPitch, " Yaw: ", curYaw)
+        print()
 
 if(__name__ == '__main__'):
     main()
