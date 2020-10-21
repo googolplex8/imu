5636c1b1fcc6022ed4c8b235bd3b84a5a44057ea
Eli Smith <googolplex8@yahoo.com>
2020-10-21 16:08:29 -0400

    finished plotting. started filter

---
+++ acc_x_filter.py
@@ -0,0 +1,31 @@
+from rover_common import aiolcm
+from rover_msgs import IMUData
+
+class LinearAccelFilter:
+    @attribute threshold
+    @attribute weight
+    @attribute prev_accel
+    @attribute cur_accel
+
+    def __init__(self, threshold, weight):
+        self.lcm = aiolcm.AsyncLCM()
+        self.lcm.subscribe("/imu")
+        
+        self.threshold = threshold
+        self.weight = weight
+
+    def update(self,prev,cur):
+        #thresholding to 0
+        if (prev_accel < threshold):
+            cur_accel = 0
+        #else, perform simple low-pass filtering
+        else:
+            cur_accel = (weight) * cur_accel + (1-weight) * prev_accel
+            prev_accel = cur_accel
+
+        self.lcm.publish('/filtered_imu', self.cur_accel)
+
+
+
+
+    
--- plotting.py
+++ plotting.py
@@ -1,69 +1,89 @@
 import matplotlib.pyplot as plt
 import csv
 
-acc_x = []
-acc_y = []
-acc_z = []
-gyr_x = []
-gyr_y = []
-gyr_z = []
-mag_x = []
-mag_y = []
-mag_z = []
+raw_acc_x = []
+raw_roll = []
+raw_pitch = []
+raw_yaw = []
+raw_bearing = []
+
+filt_acc_x = []
+filt_roll = []
+filt_pitch = []
+filt_yaw = []
+filt_bearing = []
+
+raw_length = 0
+filtered_length = 0
+
+raw_path = 'test_raw_data.txt'
+filt_path = 'test_filtered_data.txt'
 
 isFirstRow = True
-length = 0
-
-with open('test_data.txt','r') as csvfile:
+with open(raw_path,'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         if(isFirstRow):
             isFirstRow=False
         else:
-            #skip first row somehow
-            length +=1
-            acc_x.append(float(row[0]))
-            acc_y.append(float(row[1]))
-            acc_z.append(float(row[2]))
-            gyr_x.append(float(row[3]))
-            gyr_y.append(float(row[4]))
-            gyr_z.append(float(row[5]))
-            mag_x.append(float(row[6]))
-            mag_y.append(float(row[7]))
-            mag_z.append(float(row[8]))
+            raw_length +=1
+            raw_acc_x.append(float(row[0]))
+            raw_roll.append(float(row[1]))
+            raw_pitch.append(float(row[2]))
+            raw_yaw.append(float(row[3]))
+            raw_bearing.append(float(row[4]))
 
-time = range(0,length)
+isFirstRow = True
+with open(filt_path,'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        if(isFirstRow):
+            isFirstRow=False
+        else:
+            filtered_length +=1
+            filt_acc_x.append(float(row[0]))
+            filt_roll.append(float(row[1]))
+            filt_pitch.append(float(row[2]))
+            filt_yaw.append(float(row[3]))
+            filt_bearing.append(float(row[4]))
 
-fig = plt.figure()
-plt.title('IMU Data')
+raw_time = range(0,raw_length)
+filtered_time = range(0,filtered_length)
 
-ax1 = fig.add_subplot(311)
-ax1.title.set_text('Accelerometer')
-ax1.set_ylabel("m/s^2")
-line1 = ax1.plot(time, acc_x, label="x")
-line2 = ax1.plot(time, acc_y, label="y")
-line3 = ax1.plot(time, acc_z, label="z")
-ax1.tick_params(axis='x', which='both', bottom='off',top='off',labelbottom='off')
-ax1.legend((line1, line2, line3), ("x", "y", "z"))
+fig1 = plt.figure() #Accel x
+plt.title('Accel x')
+plt.ylabel("m/s^2")
+plt.plot(raw_time, raw_acc_x, label="raw accel x")
+plt.plot(filtered_time, filt_acc_x, label="filtered accel x")
+plt.legend(loc = "upper left")
 
+fig2 = plt.figure() #Roll
+plt.title('Roll')
+plt.ylabel('Radians')
+plt.plot(raw_time, raw_roll, label="raw roll")
+plt.plot(filtered_time, filt_roll, label="filtered roll")
+plt.legend(loc = "upper left")
 
-ax2 = fig.add_subplot(312)
-ax2.title.set_text('Gyroscope')
-ax2.set_ylabel("rad/s")
-ax2.tick_params(axis='x', which='both', bottom='off',top='off',labelbottom='off')
-line4 = ax2.plot(time, gyr_x)
-line5 = ax2.plot(time, gyr_y)
-line6 = ax2.plot(time, gyr_z)
-ax2.legend((line4, line5, line6), ("x", "y", "z"))
+fig3 = plt.figure() #Pitch
+plt.title('Pitch')
+plt.ylabel('Radians')
+plt.plot(raw_time, raw_pitch, label = "raw pitch")
+plt.plot(filtered_time, filt_pitch, label = "filtered pitch")
+plt.legend(loc = "upper left")
 
+fig4 = plt.figure() #Yaw
+plt.title('Yaw')
+plt.ylabel('Radians')
+plt.plot(raw_time, raw_yaw, label = "raw yaw")
+plt.plot(filtered_time, filt_yaw, label = "filtered yaw")
+plt.legend(loc = "upper left")
 
-ax3 = fig.add_subplot(313)
-ax3.title.set_text('Magnetometer')
-ax3.set_ylabel("uT")
-line7 = ax3.plot(time, mag_x)
-line8 = ax3.plot(time, mag_y)
-line9 = ax3.plot(time, mag_z)
-ax3.legend((line7, line8, line9), ("x", "y", "z"))
+fig5 = plt.figure() #Bearing
+plt.title('Bearing')
+plt.ylabel('Degrees')
+plt.plot(raw_time, raw_bearing, label = "raw bearing")
+plt.plot(filtered_time, filt_bearing, label = "filtered bearing")
+plt.legend(loc = "upper left")
 
 
 plt.show()
